c1277518092be8df094ff1cf041f370c073b6e61
Chenfeng Zhu <zhuchenf@gmail.com>
2019-09-02 15:47:15 +0200

    ITC testing project: init

---
+++ src/main/java/org/zhuzhu/itc/geows/ItcGeoWsApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 - Chenfeng Zhu.
+ */
+package org.zhuzhu.itc.geows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Chenfeng Zhu
+ *
+ */
+@SpringBootApplication
+public class ItcGeoWsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItcGeoWsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/zhuzhu/itc/geows/ItcGeoWsApplicationTests.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019 - Chenfeng Zhu.
+ */
+package org.zhuzhu.itc.geows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 
+ * @author Chenfeng Zhu
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItcGeoWsApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+
+}
